7b78061424e1ea4c0416f0e4f1aa5239dd3298c1
rafaeldiazmiles <rafaeldiazmiles@gmail.com>
2022-01-09 13:09:12 -0300

    fixing

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"html"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -11,7 +15,26 @@ func main() {
 		urlPathElements := strings.Split(r.URL.Path, "/")
 		// If request is GET with correct syntax
 		if urlPathElements[1] == "roman_number" {
-
+			number, _ := strconv.Atoi(strings.TrimSpace(urlPathElements[2]))
+			if number == 0 || number > 10 {
+				// If resource is no in the list, send Not Found Status
+				w.WriteHeader(http.StatusNotFound)
+				w.Write([]byte("404 - Not Found"))
+			} else {
+				fmt.Fprintf(w, "%q", html.EscapeString(romanNumerals.Numerals[number]))
+			}
+		} else {
+			// For all other requests, tell that Client sent a bad request
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("400 - Bad Request"))
 		}
 	})
+	// Create a server and run it on 8000 port
+	s := &http.Server{
+		Addr:           ":8000",
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
 }
