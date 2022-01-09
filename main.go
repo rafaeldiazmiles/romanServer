206838edaef1eacaa2c185ff550dc91aac3da3c6
rafaeldiazmiles <rafaeldiazmiles@gmail.com>
2022-01-09 12:53:47 -0300

    first commit

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"strings"
+)
 
+func main() {
+	// htttp package has methods for dealing with requests
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		urlPathElements := strings.Split(r.URL.Path, "/")
+		// If request is GET with correct syntax
+		if urlPathElements[1] == "roman_number" {
+
+		}
+	})
 }
